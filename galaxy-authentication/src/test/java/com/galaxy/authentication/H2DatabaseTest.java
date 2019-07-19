6e7a770bf039c06deba1f66013d0b8f403be80e0
何民明 <minming.he@anxintrust.com>
2019-07-19 11:45:04 +0800

    新增表结构；嵌入h2数据库配置

--- galaxy-authentication/src/test/java/com/galaxy/authentication/H2DatabaseTest.java
+++ galaxy-authentication/src/test/java/com/galaxy/authentication/H2DatabaseTest.java
@@ -1,73 +1,49 @@
 package com.galaxy.authentication;
 
-import org.h2.jdbcx.JdbcDataSource;
-import org.junit.After;
+import com.galaxy.authentication.domain.entity.User;
+import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.ResourceUtils;
 
 import java.io.*;
 import java.net.URLDecoder;
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class H2DatabaseTest {
 
-    private Connection connection;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Before
     public void startup() throws IOException, SQLException {
-        JdbcDataSource jdbcDataSource = new JdbcDataSource();
-        jdbcDataSource.setUrl("jdbc:h2:mem:corm");
-        jdbcDataSource.setUser("sa");
-        jdbcDataSource.setPassword("");
-        try {
-            connection = jdbcDataSource.getConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
         executeFromFile("tuser.sql");
     }
 
-    @After
-    public void shutdown() {
-        try {
-            this.connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Ignore
     @Test
-    public void query() throws SQLException {
-        Statement statement = this.connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from tuser");
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString(1));
-            System.out.println(resultSet.getString(2));
-        }
-        statement.close();
+    public void queryUser(){
+        RowMapper<User> rowMapper = new BeanPropertyRowMapper<>(User.class);
+        List<User> userList = jdbcTemplate.query("select * from tuser", rowMapper);
+        Assert.assertFalse(userList.isEmpty());
     }
-
 
     public void executeFromFile(String fileName) throws SQLException, IOException {
-        Statement statement = this.connection.createStatement();
         String sqlPath = ResourceUtils.getURL("classpath:").getPath() + "\\sql\\";
         sqlPath = URLDecoder.decode(sqlPath, "utf-8") + fileName;
         String sqlStr = readToString(sqlPath);
-        statement.execute(sqlStr);
-        statement.close();
+        this.jdbcTemplate.execute(sqlStr);
     }
 
     private String readToString(String fileName) {
--- galaxy-authentication/src/test/java/com/galaxy/authentication/controller/UserControllerTest.java
+++ galaxy-authentication/src/test/java/com/galaxy/authentication/controller/UserControllerTest.java
@@ -1,19 +1,36 @@
 package com.galaxy.authentication.controller;
 
+import com.galaxy.authentication.domain.entity.User;
+import com.galaxy.common.exception.BusinessException;
+import com.galaxy.common.rest.domain.JsonResult;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Objects;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class UserControllerTest {
 
-    @Test
-    public void getUsersTest() {
+    @Autowired
+    private UserController userController;
 
+    @Test
+    @WithMockUser(username = "richard.qian")
+    public void getUsersTest() {
+        try {
+            JsonResult<User> jsonResult = userController.getCurrentUser();
+            Assert.assertTrue(Objects.nonNull(jsonResult.getObject()));
+        } catch (BusinessException e) {
+            e.printStackTrace();
+        }
     }
 
 }
