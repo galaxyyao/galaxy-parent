3ae52cc9ab427d695a0936e053494d2221bbc1c7
何民明 <minming.he@anxintrust.com>
2019-07-19 09:31:41 +0800

    解决权限控制层方法映射冲突问题；添加H2数据库单元测试；添加测试配置文件；添加tuser表的sql文件

---
+++ galaxy-authentication/src/test/java/com/galaxy/authentication/controller/UserControllerTest.java
@@ -0,0 +1,19 @@
+package com.galaxy.authentication.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public class UserControllerTest {
+
+    @Test
+    public void getUsersTest() {
+
+    }
+
+}
